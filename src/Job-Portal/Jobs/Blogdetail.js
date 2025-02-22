23167777a5866ea805afe816e4b0cada8e8440f8
nikitakumari1309 <nikita.mca.lnct.2022@gmail.com>
2025-02-22 16:40:35 +0530

    UpButton

--- src/Job-Portal/Jobs/Blogdetail.js
+++ src/Job-Portal/Jobs/Blogdetail.js
@@ -16,6 +16,8 @@ import "./Allobs.module.css"
 import HTMLReactParser from 'html-react-parser'
 import StProfile from "../Profile/StudentProfile"
 import EMpProfile from "../Profile/EmployeeProfile"
+import Down from '../img/icons8-down-button-24.png'
+import Up from '../img/icons8-arrow-button-24.png'
 
 function Answerdetails(props) {
   
@@ -135,31 +137,49 @@ async function deletComment(id){
     getjobs()
   }, [])
 
+  function goUp(){
+    window.scrollTo({
+      top:0,
+      behavior:"smooth"
+    })
+  }
+  function goDown(){
+    window.scrollTo(50,5000000)
 
+    }      
   
   return (
     <>
-
-        <h2 style={{marginLeft:"10px", fontWeight:"800", marginTop:"15px", marginBottom:"-15px"}}> Blogs  </h2>
-
-    <div style={{display:"flex", marginTop:"20px"}}>
+        
+    {/* <div style={{display:"flex", marginTop:"20px"}}> */}
                             {/* <img style={{ height:"25px", color:"grey", marginTop:"20px", marginLeft:"8%", cursor:"pointer",
              width:"28px"}} onClick={()=>{navigate(-1)}}  src={Arrowimage} /> */}
              {/* <button style={{  color:"grey", marginTop:"10px", marginLeft:"8%", cursor:"pointer",}} 
              onClick={()=>{navigate(-1)}}>Back</button> */}
-             <button class={styles.jobdetailBackBtn} onClick={()=>{navigate(-1)}}>Back</button>
+             {/* <button class={styles.jobdetailBackBtn} onClick={()=>{navigate(-1)}}>Back</button> */}
               
-    </div>
+    {/* </div> */}
 
       {screenSize.width>850 ?
 
         <>
-    
-  
-  <h1 style={{textAlign:"center", fontSize:"40px"}}>{jobs?.jobTitle?jobs.jobTitle.charAt(0).toUpperCase()+jobs.jobTitle.substring(1):"Loading..."}</h1>
-<div style={{marginLeft:"30px"}}>
-  <span>Posted by {jobs.name}</span> |  
-  <span> Posted on : {new Date(jobs.createdAt).toLocaleString(
+    <div class={ styles.blogArrow} style={{display:"flex", height:"50px", alignItems:"center"}}>
+           <h2 style={{fontSize:"25px",marginLeft:"120px", fontWeight:"800", marginTop:"-15px", marginBottom:"-15px"}}> Blogs  </h2>
+          <img style={{marginLeft:"35%", height: "30px"}}  onClick={()=>{goDown()}} src={Down}/>
+        </div>
+        
+    <div class={styles.readPageContainer}>
+       <div class={styles.ReadPageBtnTitleContainer} style={{display:"flex"}}>
+           <button class={styles.readPageBackBtn} onClick={()=>{navigate(-1)}}>Back</button>
+     
+              <h1 style={{textAlign:"center", fontSize:"40px", whiteSpace:"no", marginTop:"10px",marginRight:"120px"}}>{jobs?.jobTitle?jobs.jobTitle.charAt(0).toUpperCase()+jobs.jobTitle.substring(1):"Loading..."}</h1>
+           
+           {/* <button class={styles.readPageBackBtn} onClick={()=>{navigate(-1)}} style={{display:"none"}}>Share</button> */}
+
+      </div>    
+              <div style={{marginLeft:"12px"}}>
+                <span>Posted by {jobs.name}</span> |  
+                <span> Posted on : {new Date(jobs.createdAt).toLocaleString(
                   "en-US",
                   {
                     month: "short",
@@ -167,8 +187,8 @@ async function deletComment(id){
                     year: "numeric",
                   }
                 )}</span> . 
-  
-</div>
+              
+     </div>
 
 
   <table style={{marginLeft:"6px", marginTop:"0px", width:"98.8%", borderCollapse: "collapse",border:"none"}}>         
@@ -182,6 +202,8 @@ async function deletComment(id){
 
   </tr>
   </table>
+  </div>
+  <img style={{marginLeft:"50%",height:"30px",marginBottom:"20px" }}  onClick={()=>{goUp()}} src={Up}/>
           </>
           :
           <>
@@ -189,6 +211,10 @@ async function deletComment(id){
 
 
               <>
+              <div class={styles.mobileReadTopbtnsContainer}>
+              <button class={styles.readPageBackBtn} onClick={()=>{navigate(-1)}}>Back</button>
+              <img style={{marginLeft:"20%",height:"30px",marginTop:"10px" }}  onClick={()=>{goDown()}} src={Down}/>
+              </div>
                 <div className={styles.JobCard} >
                 <p className={`${styles.Date} ${styles.readPageDate}`}>{new Date(jobs.createdAt).toLocaleString(
           "en-US",
@@ -219,9 +245,13 @@ async function deletComment(id){
      } 
     </td>
 
+
   </tr>
-  </table>           
+  </table>  
+
                 </div>
+                <img style={{marginLeft:"50%",height:"30px",marginTop:"10px" }}  onClick={()=>{goUp()}} src={Up}/>
+             
               </>
 
             </div>
